8c41097eb1695c2919f3d0e30bf1f610d83c0c5e
VISAVADIYA KEYUR <104882712+2104030101120@users.noreply.github.com>
2023-11-03 02:18:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById('side_cart');
+var display=0;
+
+function cartshow(){
+if(display==1){
+    div.style.display="block"
+    display=0;
+}
+ else{
+    div.style.display="block"
+    display=1;
+ }
+}
